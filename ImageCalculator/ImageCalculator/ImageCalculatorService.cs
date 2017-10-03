0b80f8a3d079e19c37d26e060a2c06a6900182c4
drumCadence <drumCadence@outlook.com>
2017-10-03 10:06:44 -0600

    master: Added Console and UnitTest projects to solution. Renamed existing namespaces.

---
+++ ImageCalculator/ImageCalculator/ImageCalculatorService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ImageCalculatorServices
+{
+    public class ImageCalculatorService
+    {
+    }
+}
---
+++ ImageCalculator/ImageCalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImageCalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageCalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf04abf9-db3b-4131-b076-270003297ced")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
