012de184a42dd87c65269079b5d530dc4fb46b01
drumCadence <drumCadence@outlook.com>
2017-10-04 09:58:30 -0600

    refactor: Created ICalculator classes. Refeactored Form.

---
+++ ImageCalculator/ImageCalculator.Service/Calculators/CalculatorFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCalculator.Service.Calculators
+{
+    public class CalculatorFactory
+    {
+        //
+        // TODO:...
+        //
+        public CalculatorFactory()
+        {
+
+        }
+    }
+}
