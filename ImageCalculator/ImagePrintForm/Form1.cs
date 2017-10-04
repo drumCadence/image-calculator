6446985a39c8cdbbcc6f0f4533693dc49aa53653
drumCadence <drumCadence@outlook.com>
2017-10-04 08:31:12 -0600

    static-rows: refactor.

--- ImageCalculator/ImagePrintForm/Form1.cs
+++ ImageCalculator/ImagePrintForm/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -45,6 +40,7 @@ namespace ImagePrintForm
 
             var evenDictRow = new Dictionary<string, List<Vertex>>();
 
+            // TODO: TO REPLACE WITH DYNAMIC ROWS AND COLUMNS...
             PrintRowA(startRow);
             PrintRowB(startRow++);
             PrintRowC(startRow++);
@@ -52,81 +48,8 @@ namespace ImagePrintForm
             PrintRowE(startRow++);
             PrintRowF(startRow++);
 
-            #region Commented
-            //for (int row = 0; row < 6; row++)
-            //{
-            //var vertexes = new List<Vertex>();
-
-            //// even colums
-            //int col = 1;
-            //while (col <= 6)
-            //{
-            //    char rowName = (char)(startRow + col);
-
-            //    // points represent V1, V2, and V3
-            //    Point[] t0 = { new Point(50 * col, 0), new Point(50 * col, 50), new Point(0 * col, 0) };
-            //    vertexes.Add(new Vertex { Name = $"{rowName}{2}", Points = t0, Pen = pen1 });
-
-            //    Point[] t2 = { new Point(100 * col, 0), new Point(100 * col, 50), new Point(50 * col, 0) };
-            //    vertexes.Add(new Vertex { Name = $"{rowName}{4}", Points = t2, Pen = pen1 });
-
-            //    Point[] t4 = { new Point(150 * col, 0), new Point(150 * col, 50), new Point(100 * col, 0) };
-            //    vertexes.Add(new Vertex { Name = $"{rowName}{6}", Points = t4, Pen = pen1 });
-
-            //    Point[] t6 = { new Point(200 * col, 0), new Point(200 * col, 50), new Point(150 * col, 0) };
-            //    vertexes.Add(new Vertex { Name = $"{rowName}{8}", Points = t6, Pen = pen1 });
-
-            //    Point[] t8 = { new Point(250 * col, 0), new Point(250 * col, 50), new Point(200 * col, 0) };
-            //    vertexes.Add(new Vertex { Name = $"{rowName}{10}", Points = t8, Pen = pen1 });
-
-            //    Point[] t10 = { new Point(300 * col, 0), new Point(300 * col, 50), new Point(250 * col, 0) };
-            //    vertexes.Add(new Vertex { Name = $"{rowName}{12}", Points = t10, Pen = pen1 });
-
-            //    rowFactor += 50;
-            //    col++;
-            //}
-
-            //for (int r = 0; r < 6; r++)
-            //{
-            //    g = pictureBox1.CreateGraphics();
-            //    g.DrawPolygon(vertexes[r].Pen, vertexes[r].Points);
-            //}
-
-
-            //var vertexes2 = new List<Vertex>();
-            //col = 1;
-            //while (col <= 6)
-            //{
-            //    var rowName = (char)(startRow + col);
-
-            //    Point[] t1 = { new Point(0, 50), new Point(0, 0), new Point(50, 50) };
-            //    vertexes2.Add(new Vertex { Name = $"{rowName}{1}", Points = t1, Pen = pen2 });
-
-            //    Point[] t3 = { new Point(50, 50), new Point(50, 0), new Point(100, 50) };
-            //    vertexes2.Add(new Vertex { Name = $"{rowName}{3}", Points = t3, Pen = pen2 });
-
-            //    Point[] t5 = { new Point(100, 50), new Point(100, 0), new Point(150, 50) };
-            //    vertexes2.Add(new Vertex { Name = $"{rowName}{5}", Points = t5, Pen = pen2 });
-
-            //    Point[] t7 = { new Point(150, 50), new Point(150, 0), new Point(200, 50) };
-            //    vertexes2.Add(new Vertex { Name = $"{rowName}{7}", Points = t7, Pen = pen2 });
-
-            //    Point[] t9 = { new Point(200, 50), new Point(200, 0), new Point(250, 50) };
-            //    vertexes2.Add(new Vertex { Name = $"{rowName}{9}", Points = t9, Pen = pen2 });
-
-            //    Point[] t11 = { new Point(250, 50), new Point(250, 0), new Point(300, 50) };
-            //    vertexes2.Add(new Vertex { Name = $"{rowName}{11}", Points = t11, Pen = pen2 });
-
-            //    col++;
-            //}
-
-            //for (int r = 0; r < 6; r++)
-            //{
-            //    g = pictureBox1.CreateGraphics();
-            //    g.DrawPolygon(vertexes2[r].Pen, vertexes2[r].Points);
-            //}
-            //}
-            #endregion
+            var tw = TriangleWidth(50);
+            Debug.WriteLine("tw = {0}", tw);
         }
 
 
@@ -295,7 +218,7 @@ namespace ImagePrintForm
 
                 Point[] t4 = { new Point(100, 150), new Point(100, 100), new Point(150, 150) };
                 vertexes.Add(new Vertex { Name = $"{rowName}{6}", Points = t4, Pen = pen1 });
-                
+
                 Point[] t6 = { new Point(150, 150), new Point(150, 100), new Point(200, 150) };
                 vertexes.Add(new Vertex { Name = $"{rowName}{8}", Points = t6, Pen = pen1 });
 
@@ -365,7 +288,7 @@ namespace ImagePrintForm
 
                 Point[] t2 = { new Point(50, 200), new Point(50, 150), new Point(100, 200) };
                 vertexes.Add(new Vertex { Name = $"{rowName}{4}", Points = t2, Pen = pen1 });
-                
+
                 Point[] t4 = { new Point(100, 200), new Point(100, 150), new Point(150, 200) };
                 vertexes.Add(new Vertex { Name = $"{rowName}{6}", Points = t4, Pen = pen1 });
 
@@ -403,7 +326,7 @@ namespace ImagePrintForm
 
                 Point[] t5 = { new Point(150, 150), new Point(150, 200), new Point(100, 150) };
                 vertexes2.Add(new Vertex { Name = $"{rowName}{5}", Points = t5, Pen = pen2 });
-                
+
                 //
                 Point[] t7 = { new Point(200, 150), new Point(200, 200), new Point(150, 150) };
                 vertexes2.Add(new Vertex { Name = $"{rowName}{7}", Points = t7, Pen = pen2 });
@@ -570,6 +493,13 @@ namespace ImagePrintForm
                 g = pictureBox1.CreateGraphics();
                 g.DrawPolygon(vertexes2[r].Pen, vertexes2[r].Points);
             }
+        }
+    
+
+        // Return the width of a triangle.
+        private float TriangleWidth(float height)
+        {
+            return (float)(2 * height / Math.Sqrt(3));
         }
     }
 }
