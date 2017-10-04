d21856b8b0e9926872896fd957ecb09ad95de865
drumCadence <drumCadence@outlook.com>
2017-10-04 00:25:19 -0600

    static-rows: More refactoring.

--- ImageCalculator/ImagePrintForm/Form1.cs
+++ ImageCalculator/ImagePrintForm/Form1.cs
@@ -41,6 +41,7 @@ namespace ImagePrintForm
         {
             Pen pen1 = new Pen(Color.Blue, 2);
             Pen pen2 = new Pen(Color.Red, 2);
+            int startRow = 65;
 
             var evenDictRow = new Dictionary<string, List<Vertex>>();
 
@@ -52,64 +53,69 @@ namespace ImagePrintForm
                 int i = 0;
                 while (i <= 6)
                 {
+                    char rowName = (char)(startRow + i);
+
                     // points represent V1, V2, and V3
                     Point[] t0 = { new Point(50, 0), new Point(50, 50), new Point(0, 0) };
-                    vertexes.Add(new Vertex { Name = $"A{i + 2}", Points = t0, Pen = pen1 });
+                    vertexes.Add(new Vertex { Name = $"{rowName}{2}", Points = t0, Pen = pen1 });
 
                     Point[] t2 = { new Point(100, 0), new Point(100, 50), new Point(50, 0) };
-                    vertexes.Add(new Vertex { Name = $"A{i + 4}", Points = t2, Pen = pen1 });
+                    vertexes.Add(new Vertex { Name = $"{rowName}{4}", Points = t2, Pen = pen1 });
 
                     Point[] t4 = { new Point(150, 0), new Point(150, 50), new Point(100, 0) };
-                    vertexes.Add(new Vertex { Name = $"A{i + 6}", Points = t4, Pen = pen1 });
+                    vertexes.Add(new Vertex { Name = $"{rowName}{6}", Points = t4, Pen = pen1 });
 
                     Point[] t6 = { new Point(200, 0), new Point(200, 50), new Point(150, 0) };
-                    vertexes.Add(new Vertex { Name = $"A{i + 8}", Points = t6, Pen = pen1 });
+                    vertexes.Add(new Vertex { Name = $"{rowName}{8}", Points = t6, Pen = pen1 });
 
                    
                     Point[] t8 = { new Point(250, 0), new Point(250, 50), new Point(200, 0) };
-                    vertexes.Add(new Vertex { Name = $"A{i + 10}", Points = t8, Pen = pen1 });
+                    vertexes.Add(new Vertex { Name = $"{rowName}{10}", Points = t8, Pen = pen1 });
 
                     Point[] t10 = { new Point(300, 0), new Point(300, 50), new Point(250, 0) };
-                    vertexes.Add(new Vertex { Name = $"A{i + 12}", Points = t10, Pen = pen1 });
-
+                    vertexes.Add(new Vertex { Name = $"{rowName}{12}", Points = t10, Pen = pen1 });
+                    
                     i++;
                 }
 
-                foreach(var vortex in vertexes)
+                for (int r = 0; r < 6; r++)
                 {
                     g = pictureBox1.CreateGraphics();
-                    g.DrawPolygon(vortex.Pen, vortex.Points);
+                    g.DrawPolygon(vertexes[r].Pen, vertexes[r].Points);
                 }
+
 
                 var vertexes2 = new List<Vertex>();
                 i = 0;
                 while (i <= 6)
                 {
-                    Point[] t1 = {new Point(0, 50),new Point(0, 0),new Point(50, 50)};
-                    vertexes2.Add(new Vertex { Name = $"A{i +1}", Points = t1, Pen = pen2 });
+                    var rowName = (char)(startRow + i);
 
-                    Point[] t3 = {new Point(50, 50), new Point(50, 0),new Point(100, 50)};
-                    vertexes2.Add(new Vertex { Name = $"A{i + 3}", Points = t3, Pen = pen2 });
+                    Point[] t1 = { new Point(0, 50), new Point(0, 0), new Point(50, 50) };
+                    vertexes2.Add(new Vertex { Name = $"{rowName}{1}", Points = t1, Pen = pen2 });
 
-                    Point[] t5 = {new Point(100, 50),new Point(100, 0),new Point(150, 50)};
-                    vertexes2.Add(new Vertex { Name = $"A{i + 5}", Points = t5, Pen = pen2 });
+                    Point[] t3 = { new Point(50, 50), new Point(50, 0), new Point(100, 50) };
+                    vertexes2.Add(new Vertex { Name = $"{rowName}{3}", Points = t3, Pen = pen2 });
 
-                    Point[] t7 = {new Point(150, 50),new Point(150, 0),new Point(200, 50)};
-                    vertexes2.Add(new Vertex { Name = $"A{i + 7}", Points = t7, Pen = pen2 });
+                    Point[] t5 = { new Point(100, 50), new Point(100, 0), new Point(150, 50) };
+                    vertexes2.Add(new Vertex { Name = $"{rowName}{5}", Points = t5, Pen = pen2 });
 
-                    Point[] t9 = {new Point(200, 50),new Point(200, 0),new Point(250, 50)};
-                    vertexes2.Add(new Vertex { Name = $"A{i + 9}", Points = t9, Pen = pen2 });
+                    Point[] t7 = { new Point(150, 50), new Point(150, 0), new Point(200, 50) };
+                    vertexes2.Add(new Vertex { Name = $"{rowName}{7}", Points = t7, Pen = pen2 });
 
-                    Point[] t11 = {new Point(250, 50),new Point(250, 0),new Point(300, 50)};
-                    vertexes2.Add(new Vertex { Name = $"A{i + 11}", Points = t11, Pen = pen2 });
+                    Point[] t9 = { new Point(200, 50), new Point(200, 0), new Point(250, 50) };
+                    vertexes2.Add(new Vertex { Name = $"{rowName}{9}", Points = t9, Pen = pen2 });
+
+                    Point[] t11 = { new Point(250, 50), new Point(250, 0), new Point(300, 50) };
+                    vertexes2.Add(new Vertex { Name = $"{rowName}{11}", Points = t11, Pen = pen2 });
 
                     i++;
                 }
 
-                foreach (var vortex in vertexes2)
+                for (int r = 0; r < 6; r++)
                 {
                     g = pictureBox1.CreateGraphics();
-                    g.DrawPolygon(vortex.Pen, vortex.Points);
+                    g.DrawPolygon(vertexes2[r].Pen, vertexes2[r].Points);
                 }
             }
 
